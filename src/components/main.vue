3582b8048c25919e7991e30dd4163b0755fcab27
akat32 <baduk32127@gmail.com>
2018-11-09 17:19:09 +0900

    Vue.js setting

---
+++ src/components/main.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+  </div>
+</template>
+<script>
+// import Vue from 'vue'
+import axios from 'axios'
+import XLSX from 'xlsx'
+
+export default {
+  name: 'main'
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+body{
+  margin : 0;
+}
+
+</style>
